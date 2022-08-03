136c8e64cf721a48aac435398c2dca42248a6537
JIHYE <butter1398@inha.edu>
2022-08-03 21:03:06 +0900

    Create week05_01_6044.py
    5주차 1차 과제입니다.

---
+++ JIHYE/week5_1/week05_01_6044.py
@@ -0,0 +1,15 @@
+﻿a, b = input().split()
+a = int(a)
+b = int(b)
+c1 = a + b
+c2 = a - b
+c3 = a * b
+c4 = a // b
+c5 = a % b
+c6 = a / b
+print(c1)
+print(c2)
+print(c3)
+print(c4)
+print(c5)
+print(format(c6, ".2f"))
